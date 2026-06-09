f66a2a804cb165b4619cef5c21ac8495378ed83b
ogabek <saydullayevogabek515@gmail.com>
2026-06-09 21:10:55 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'saydullayevogabek616@gmail.com'
-EMAIL_HOST_PASSWORD = 'buba juvb gfwk fnjy'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
